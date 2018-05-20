0bb6b9cd58b7c5589ab52ac19bb57597b5663467
Mariia_Ganza <mariia_ganza@epam.com>
2018-05-20 16:02:17 +0300

    eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    const info ="fff"
-    return (
-      <div>
-        <h1>Now we are ready to go!</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Now we are ready to go!</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
