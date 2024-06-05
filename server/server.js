7c6cd9eafe5ddfda86bf5ca2aabb897b2e68b482
Maria Ganza <mariia_ganza@protonmail.com>
2024-06-05 12:14:43 +0200

    feat: use apollo server with express

--- server/server.js
+++ server/server.js
@@ -1,15 +1,35 @@
 require("dotenv").config();
 const { initDB } = require("./src/util/initDB");
-
-const { createServer } = require("http");
-const app = require("./src/app");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const express = require("express");
 
 const PORT = process.env.PORT || 4000;
 
-const server = createServer(app);
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-initDB(() => {
-  server.listen(PORT, () => {
+async function startApolloServer() {
+  const app = express();
+
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
+
+  const server = new ApolloServer({ schema });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
     console.log(`server is ready on port ${PORT}🚀`);
   });
+}
+
+initDB(() => {
+  startApolloServer();
 }).catch((err) => console.error(err));
