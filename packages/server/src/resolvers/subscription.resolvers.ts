eafcd87e4a10ebdb4462334a5b1992e90b9e6f09
Maria Ganza <mariia_ganza@protonmail.com>
2024-10-08 11:33:15 +0200

    fix(server): fix comment

--- packages/server/src/resolvers/subscription.resolvers.ts
+++ packages/server/src/resolvers/subscription.resolvers.ts
@@ -5,7 +5,7 @@ const pubsub = new PubSub();
 
 export const subscriptionResolvers: SubscriptionResolvers = {
   flightScheduled: {
-    // @ts-ignore
+    // @ts-expect-error: it should work according to the docs https://www.apollographql.com/docs/apollo-server/data/subscriptions#switching-from-subscriptions-transport-ws
     subscribe: () => pubsub.asyncIterator(["FLIGHT_SCHEDULED"]),
   },
 };
