7348a005041f5abdb7fdc93df11bafa09c4bba5e
Maria Ganza <mariia_ganza@protonmail.com>
2025-01-19 17:02:19 +0100

    test: broke test

--- packages/client/cypress/e2e/flightsPage.cy.ts
+++ packages/client/cypress/e2e/flightsPage.cy.ts
@@ -46,7 +46,10 @@ describe("Flights page", () => {
   });
 
   it("validates flight form", () => {
-    cy.dataCy("flight-form").find("button").contains("Schedule flight").click();
+    cy.dataCy("flight-form")
+      .find("button")
+      .contains("Schedule flight 3")
+      .click();
     requiredFields.forEach((field) => {
       cy.dataCy(`${field}-error`)
         .should("be.visible")
